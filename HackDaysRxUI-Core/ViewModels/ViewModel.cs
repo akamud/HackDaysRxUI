ba4d828d41a17fa2b3184d1fcdc82afaf7d1c8ec
Mahmoud Ali <muddibr@gmail.com>
2016-05-18 01:38:10 -0300

    initial commit

---
+++ HackDaysRxUI-Core/ViewModels/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ReactiveUI;
+
+namespace HackDaysRxUICore
+{
+	public class ViewModel : ReactiveObject
+	{
+		private string _userName;
+
+		public string UserName {
+			get { return _userName; }
+			set { this.RaiseAndSetIfChanged(ref _userName, value); }
+		}
+	}
+}
+
---
+++ HackDaysRxUI-Droid/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using HackDaysRxUICore;
+using ReactiveUI;
+
+namespace HackDaysRxUIDroid
+{
+	[Activity (Label = "HackDaysRxUI-Droid", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : ReactiveActivity<ViewModel>
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			SetContentView (Resource.Layout.Main);
+
+			//Button button = FindViewById<Button> (Resource.Id.myButton);
+
+
+
+			ViewModel = new ViewModel();
+			this.Bind (ViewModel, v => v.UserName, x => x.UserNameEditText.Text);
+			this.Bind (ViewModel, v => v.UserName, x => x.UserName.Text);
+		}
+
+		public EditText UserNameEditText {
+			get { return this.GetControl<EditText> (); }
+		}
+
+		public TextView UserName {
+			get { return this.GetControl<TextView> (); }
+		}
+	}
+}
+
+
